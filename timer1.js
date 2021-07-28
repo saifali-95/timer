902d225e0153acbdefe0c125574aa9acc19781fd
Saif Ali <saifali7646@gmail.com>
2021-07-28 01:38:47 +0000

    initial release

---
+++ timer1.js
@@ -0,0 +1,21 @@
+let input = process.argv.slice(2);
+let toNumbers = input => input.map(Number);
+let inputNumbers= toNumbers(input);
+let arrangedInput = inputNumbers.sort(function(a, b) {
+  return a - b;
+});
+//console.log(arrangedInput);
+let time = 1000;
+let decentTime = arrangedInput[(arrangedInput.length)-1]*1100;
+
+
+for(let i=0; i < arrangedInput.length; i++){
+  setTimeout(()=>{
+    process.stdout.write('.');
+  }, arrangedInput[i]*1000)
+}
+
+setTimeout(()=>{
+  console.log('');
+}, decentTime)
+
